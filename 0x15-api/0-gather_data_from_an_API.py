dd8cad8807c9020d29a13892ad79f1d9d5d08f08
Abdii7 <xyz9531@gmail.com>
2023-07-24 08:45:56 -0700

    modified api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-import requests  # Import the requests module to make HTTP requests
-import sys  # Import the sys module to access command-line arguments
+"""
+Python script that, using a REST API, for a given employee ID,
+returns  TODO list progress.
+"""
 
-if __name__ == '__main__':  # Check if this script is being run as the main program
-    if len(sys.argv) != 2:  # Check if the number of command-line arguments is correct
-        print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))  # Print usage instructions
-        sys.exit(1)  # Exit the program with an error code
+from requests import get
+from sys import argv
 
-    employee_id = sys.argv[1]  # Get the employee ID from the command-line argument
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)  # Construct the URL for the user data API endpoint
-    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)  # Construct the URL for the TODO list API endpoint
 
-    user_response = requests.get(user_url)  # Make an HTTP GET request to get the user data
-    todo_response = requests.get(todo_url)  # Make an HTTP GET request to get the TODO list data
+if __name__ == "__main__":
+    # Get the TODO list
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    
+    # Get the user data
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    if user_response.status_code != 200:  # Check if the user data API returned an error
-        print('User not found')  # Print an error message
-        sys.exit(1)  # Exit the program with an error code
+    # Find the employee name
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-    if todo_response.status_code != 200:  # Check if the TODO list API returned an error
-        print('TODO list not found')  # Print an error message
-        sys.exit(1)  # Exit the program with an error code
+    # Count the number of completed tasks and total tasks
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
 
-    user_data = user_response.json()  # Parse the JSON response from the user data API
-    todo_data = todo_response.json()  # Parse the JSON response from the TODO list API
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
 
-    total_tasks = len(todo_data)  # Get the total number of tasks in the TODO list
-    done_tasks = sum([1 for task in todo_data if task['completed']])  # Get the number of completed tasks in the TODO list
+    # Print the results
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
 
-    print('{} is done with tasks({}/{}):'.format(user_data['name'], done_tasks, total_tasks))  # Print a message showing how many tasks are done and how many are left
-
-    for task in todo_data:  # Loop through each task in the TODO list
-        if task['completed']:  # Check if this task is completed
-            print('\t {}'.format(task['title']))  # Print this task's title with a tab character at the beginning
+    for i in tasks:
+        print("\t {}".format(i))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python3
-import requests
-import sys
+
+"""
+Python script that exports data in the CSV format
+"""
+
+from requests import get
+from sys import argv
 import csv
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == "__main__":
+    # Get the TODO list
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-    employee_id = sys.argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
+    row = []
+    
+    # Get the user data
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url)
+    # Find the employee name
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            employee = i['username']
 
-    if user_response.status_code != 200:
-        print('User not found')
-        sys.exit(1)
+    # Write the data to a CSV file
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        writ = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-    if todo_response.status_code != 200:
-        print('TODO list not found')
-        sys.exit(1)
+        for i in data:
 
-    user_data = user_response.json()
-    todo_data = todo_response.json()
+            row = []
+            if i['userId'] == int(argv[1]):
+                row.append(i['userId'])
+                row.append(employee)
+                row.append(i['completed'])
+                row.append(i['title'])
 
-    total_tasks = len(todo_data)
-    done_tasks = sum([1 for task in todo_data if task['completed']])
-
-    print('{} is done with tasks({}/{}):'.format(user_data['name'], done_tasks, total_tasks))
-
-    with open('{}.csv'.format(employee_id), mode='w', newline='') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-
-        for task in todo_data:
-            writer.writerow({
-                'USER_ID': employee_id,
-                'USERNAME': user_data['username'],
-                'TASK_COMPLETED_STATUS': 'Yes' if task['completed'] else 'No',
-                'TASK_TITLE': task['title']
-            })
-
-    print('Data exported to {}.csv'.format(employee_id))
+                writ.writerow(row)
